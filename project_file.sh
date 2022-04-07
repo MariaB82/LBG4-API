6aa10257e5c60c7d6d953c2f634e8acf69969fc4
MariaB82 <maria.baldwin@hotmail.co.uk>
2022-04-07 14:13:17 +0000

    change to script

--- project_file.sh
+++ project_file.sh
@@ -5,4 +5,6 @@ npm install
 npm test
 #build image docker build -t node image .
 docker build -t nodeimage_maria .
+docker tag nodeimage_maria gcr.io/lbg-210322/maria:v1
+docker push gcr.io/lbg-210322/nodeimage_maria:v1 
 
